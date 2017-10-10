88f4278edfa4411c1bb0bffbc8fa1591820ffebb
Jason Juhyun Kim <mtinet@hanmail.net>
2017-10-10 23:22:19 +0900

    Update electronicMaze.ino

--- electronicMaze.ino
+++ electronicMaze.ino
@@ -1,6 +1,4 @@
 #define r 9
-#define g 10
-#define b 11
 
 int buzzer = 13;
 
@@ -8,8 +6,6 @@ void setup() {
   Serial.begin(9600);
   pinMode(buzzer, OUTPUT); 
   pinMode(r, OUTPUT);
-  pinMode(g, OUTPUT);
-  pinMode(b, OUTPUT);
 }
 
 void loop() {
@@ -21,7 +17,5 @@ void loop() {
   } else {
     digitalWrite(13, HIGH);
     digitalWrite(r, HIGH); //if you want to change color, just change r,g,b HIGH and LOW
-    digitalWrite(g, LOW);
-    digitalWrite(b, LOW);
   }
 }
